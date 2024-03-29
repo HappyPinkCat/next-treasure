c2b2fa2d3e9f7aafdccf96d34b6c6808b37771c3
lpgong <lpgong@trip.com>
2024-03-29 19:18:26 +0800

    feat: 静态生成-带数据

---
+++ pages/user.js
@@ -0,0 +1,26 @@
+export default function User(props) {
+  const { users } = props;
+  console.log(users);
+  return (
+    <div>
+      <h1>UserList</h1>
+      {users.map((user) => (
+        <ul key={user.id}>
+          <li>
+            {user.name}---{user.email}
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+}
+// If you export a function called getStaticProps (Static Site Generation) from a page, Next.js will pre-render this page at build time using the props returned by getStaticProps
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  return {
+    props: {
+      users,
+    },
+  };
+}
