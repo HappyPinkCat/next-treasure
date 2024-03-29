9ab184f58c44d6eaceb4670e5e7238b48aa92891
lpgong <lpgong@trip.com>
2024-03-29 16:42:54 +0800

    feat: 可编程导航

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 export default function Home({ id = 100 }) {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log("下订单");
+    router.push("/book/101");
+    // router.replace("/book/102"); //会替换当前页面，不能回退了
+  };
   return (
     <>
       <h1>home</h1>
@@ -12,6 +19,8 @@ export default function Home({ id = 100 }) {
       <h2>
         <Link href={`/book/${id}`}>go to bookPage id {id}</Link>
       </h2>
+      <br />
+      <button onClick={handleClick}>下订单</button>
     </>
   );
 }
