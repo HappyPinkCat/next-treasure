ff9663badda0ad42d3b6d7c553d7b4cbb0d82a0f
lpgong <lpgong@trip.com>
2024-03-29 16:10:30 +0800

    feat: 链接到其他页面Link

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <h1>home</h1>;
+import Link from "next/link";
+export default function Home({ id = 100 }) {
+  return (
+    <>
+      <h1>home</h1>
+      {/* replace 会替换url，不能回退了 */}
+      <h2>
+        <Link href="/about" replace>
+          go to aboutPage
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`/book/${id}`}>go to bookPage id {id}</Link>
+      </h2>
+    </>
+  );
 }
